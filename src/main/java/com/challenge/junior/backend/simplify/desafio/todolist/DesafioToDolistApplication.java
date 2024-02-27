13d59de91ecba18652919bcc8288da1722e0b2cf
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-02-26 22:57:11 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/com/challenge/junior/backend/simplify/desafio/todolist/DesafioToDolistApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.junior.backend.simplify.desafio.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioToDolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioToDolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/junior/backend/simplify/desafio/todolist/DesafioToDolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.junior.backend.simplify.desafio.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioToDolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
