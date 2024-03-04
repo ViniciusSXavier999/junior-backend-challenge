986a478559afb3133eb7ee484b560b0b88a95dea
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-03-04 15:02:34 -0300

    Aprimorando o código com o ResponseEntity nos endpoints da classe controller

--- src/main/java/com/challenge/junior/backend/simplify/desafio/todolist/controller/TodoController.java
+++ src/main/java/com/challenge/junior/backend/simplify/desafio/todolist/controller/TodoController.java
@@ -3,6 +3,9 @@ package com.challenge.junior.backend.simplify.desafio.todolist.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,22 +28,47 @@ public class TodoController {
 	private TodoService todoService;
 
 	@PostMapping
+	public ResponseEntity<List<Todo>> create(@RequestBody @Valid Todo todo) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(todoService.create(todo));
+	}
+	
+	/*
 	List<Todo> create(@RequestBody @Valid Todo todo) {
 		return todoService.create(todo);
 	}
+	*/
 	
 	@GetMapping
+	public ResponseEntity<List<Todo>> list() {
+		return ResponseEntity.status(HttpStatus.OK).body(todoService.list());
+	}
+	
+	
+	/*
 	List<Todo> list() {
 		return todoService.list();
 	}
+	*/
 	
 	@PutMapping
+	public ResponseEntity<List<Todo>> update(@RequestBody Todo todo) {
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(todoService.update(todo));
+	}
+	
+	/*
 	List<Todo> update(  @RequestBody Todo todo) {
 		return todoService.update(todo);
 	}
+	*/
 	
 	@DeleteMapping("{id}")
+	public ResponseEntity<List<Todo>> delete(@PathVariable Long id) {
+		return ResponseEntity.status(HttpStatus.OK).body(todoService.delete(id));
+	}
+	
+	/*
 	List<Todo> delete(@PathVariable Long id) {
 		return todoService.delete(id);
 	}
+	*/
 }
